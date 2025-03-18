caf9116df210bca9759fa0ff4e81367066d196fb
Rgit915 <rora.goitom@gmail.com>
2025-03-18 00:00:59 -0400

    Add plan data for pricing component

---
+++ data.js
@@ -0,0 +1,28 @@
+const planData = [
+  {
+    name: 'Basic',
+    priceMonthly: '$19.99',
+    priceYearly: '$199.99',
+    storageLimit: '500 GB Storage',
+    usersAllowed: '2 Users Allowed',
+    sendUp: 'Send up to 3 GB'
+  },
+  {
+    name: 'Professional',
+    priceMonthly: '$24.99',
+    priceYearly: '$249.99',
+    storageLimit: '1 TB Storage',
+    usersAllowed: '5 Users Allowed',
+    sendUp: 'Send up to 10 GB'
+  },
+  {
+    name: 'Master',
+    priceMonthly: '$39.99',
+    priceYearly: '$399.99',
+    storageLimit: '2 TB Storage',
+    usersAllowed: '10 Users Allowed',
+    sendUp: 'Send up to 20 GB'
+  }
+];
+
+export default planData;
