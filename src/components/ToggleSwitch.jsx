10eedd8a887d59055dafc39af64c6abc9c19d17d
Rgit915 <rora.goitom@gmail.com>
2025-03-25 15:19:05 -0400

    Move toggle div outside label for correct structure

--- src/components/ToggleSwitch.jsx
+++ src/components/ToggleSwitch.jsx
@@ -22,18 +22,18 @@ const ToggleSwitch = ({ isAnnually, setIsAnnually }) => {
       <label
         htmlFor="toggle"
         className="relative w-14 h-7 bg-gradient-to-r from-primary-moderate-blue to-primary-vibrant-blue rounded-full cursor-pointer flex items-center hover:opacity-50"
-      >
-        <div
-          className={`absolute w-6 h-6 bg-white rounded-full shadow-md transition-transform duration-300 ${
-            isAnnually ? "translate-x-1" : "translate-x-7"
-          }`}
-          tabIndex="0" // Make the switch focusable
-          role="switch" // Define the role as a switch for accessibility
-          aria-checked={isAnnually} // Indicate the current state of the toggle
-          aria-label="Toggle pricing plan"
-          onKeyDown={handleKeyDown} // Allow toggling with the keyboard
-        />
-      </label>
+      ></label>
+      <div
+        className={`absolute w-6 h-6 bg-white rounded-full shadow-md transition-transform duration-300 ${
+          isAnnually ? "translate-x-1" : "translate-x-7"
+        }`}
+        tabIndex="0" // Make the switch focusable
+        role="switch" // Define the role as a switch for accessibility
+        aria-checked={isAnnually} // Indicate the current state of the toggle
+        aria-label="Toggle pricing plan"
+        onKeyDown={handleKeyDown} // Allow toggling with the keyboard
+      />
+
       <span className="text-[15px] font-bold opacity-50 leading-7">
         Monthly
       </span>
