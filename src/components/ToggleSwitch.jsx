8bbcf85ac830a4a810452c13bc8a912a0dde984a
Rgit915 <rora.goitom@gmail.com>
2025-03-25 15:37:02 -0400

    Move toggle ball div out of label and ensure accessibility

--- src/components/ToggleSwitch.jsx
+++ src/components/ToggleSwitch.jsx
@@ -1,6 +1,6 @@
 const ToggleSwitch = ({ isAnnually, setIsAnnually }) => {
   const handleKeyDown = (e) => {
-    if (e.key === "Enter" || e.key === " ") {
+    if (e.key === "Enter" || e.key === "Space") {
       setIsAnnually(!isAnnually);
     }
   };
@@ -10,30 +10,22 @@ const ToggleSwitch = ({ isAnnually, setIsAnnually }) => {
       <span className="text-[15px] font-bold opacity-50 leading-7">
         Annually
       </span>
-      <input
-        type="checkbox"
-        id="toggle"
-        className="hidden"
-        checked={isAnnually}
-        onChange={() => setIsAnnually(!isAnnually)}
-        aria-checked={isAnnually} // Indicate the current state
-        aria-label="Toggle between Annual and Monthly pricing" // Label for screen readers
-      />
-      <label
-        htmlFor="toggle"
-        className="relative w-14 h-7 bg-gradient-to-r from-primary-moderate-blue to-primary-vibrant-blue rounded-full cursor-pointer flex items-center hover:opacity-50"
-      ></label>
       <div
-        className={`absolute w-6 h-6 bg-white rounded-full shadow-md transition-transform duration-300 ${
-          isAnnually ? "translate-x-1" : "translate-x-7"
-        }`}
-        tabIndex="0" // Make the switch focusable
+        className="relative w-14 h-7 bg-gradient-to-r from-primary-moderate-blue to-primary-vibrant-blue rounded-full cursor-pointer flex items-center hover:opacity-50"
+        onClick={() => setIsAnnually(!isAnnually)}
+        tabIndex="0" // Make the div focusable
         role="switch" // Define the role as a switch for accessibility
         aria-checked={isAnnually} // Indicate the current state of the toggle
         aria-label="Toggle pricing plan"
         onKeyDown={handleKeyDown} // Allow toggling with the keyboard
-      />
-
+      >
+        <div
+          className={`absolute w-6 h-6 bg-white rounded-full shadow-md transition-transform duration-300 ${
+            isAnnually ? "translate-x-1" : "translate-x-7"
+          }`}
+          aria-hidden="true"
+        />
+      </div>
       <span className="text-[15px] font-bold opacity-50 leading-7">
         Monthly
       </span>
